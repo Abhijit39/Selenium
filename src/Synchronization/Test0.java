175a80f6c5c99a9416798d03c53d242b66b6f75e
ABHIJIT <ABHIJIT@ABHIJIT>
2022-12-11 18:39:38 -0800

    sy

---
+++ src/Synchronization/Test0.java
@@ -0,0 +1,14 @@
+package Synchronization;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+	public static void main(String[] arg)
+	{
+		WebDriver driver=new ChromeDriver();
+		
+	}
+
+}
+
